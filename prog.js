475516f472b5c81526537451b7af3b0c215f584c
AbbyNew <x.nouvelle@live.fr>
2021-03-30 10:45:29 +0200

    Modifs pour parser l'ensemble des éléments de l'objet  ramené par la requête

--- prog.js
+++ prog.js
@@ -2,38 +2,66 @@
 const boutonAskWeather = document.getElementById('ask-weather');
 
 // texte qui donne le resultat de la requete
-let weatherResult = document.getElementById("weather-result");
+let weatherResult = document.getElementById('weather-result');
 
-
-// fonction d'affichage du résultat, appelée par la requete quand elle change de statut 
-function affichResult(ResString) {
-    weatherResult.innerText=ResString ; 
-} ; 
-
-// fonction de requete AJAX
-const askWeather = function (actualStatus) {
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      var response = JSON.parse(this.responseText);
-      console.log(response.current_condition.condition);
-      affichResult(response.current_condition.condition) ; 
-    }
-    
-    //if (actualStatus != this.status) {
-    //    alert("Dans la fonction" + this.status + " act sts: " + actualStatus);
-    //    actualStatus = this.status;
-    //}
-        //console.log(this.status) ;
-  };
-  request.open("GET", "https://www.prevision-meteo.ch/services/json/paris");
-  request.send();
+// fonction permettant de savoir la taille de l'objet
+Object.size = function(obj) {
+	var size = 0,
+		key;
+	for (key in obj) {
+		if (obj.hasOwnProperty(key)) size++;
+	}
+	return size;
 };
 
-// Action sur le bouton, qui  appelle la fonction de requete AJAX
-boutonAskWeather.addEventListener("click", function () {
-  var actualStatus = 0;
-  askWeather(actualStatus);
+// fonction d'affichage du résultat, appelée par la requete quand elle change de statut
+function affichResult(ResString) {
+	weatherResult.innerHTML = ResString;
+}
+
+// Affichage de l'objet ResObj - il faut le "parser"
+function affichResults(ResObj) {
+	globString = '';
+	for (const [ key, value ] of Object.entries(ResObj)) {
+		//console.log(`${key}: ${value}`) ;
+		globString += `${key}`;
+		// value: object or something else?
+		var size = Object.size(value);
+		if (size > 1) {
+			globString += ' -> ' + size + ' elements \n'; 
+
+			for (const [ key2, value2 ] of Object.entries(value)) {
+				globString += '<li>' + `${key2}` + '<span>\t' +  `${value2}` + '</span></li>';
+			}
+		} else globString += `${value}` + '\n';
+
+		affichResult(globString);
+	}
+}
+
+// fonction de requete AJAX
+const askWeather = function(actualStatus) {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			var response = JSON.parse(this.responseText);
+			//console.log(response.current_condition.condition);
+			//affichResult(response.current_condition.condition) ;
+			affichResults(response);
+		}
+
+		//if (actualStatus != this.status) {
+		//    alert("Dans la fonction" + this.status + " act sts: " + actualStatus);
+		//    actualStatus = this.status;
+		//}
+		//console.log(this.status) ;
+	};
+	request.open('GET', 'https://www.prevision-meteo.ch/services/json/paris');
+	request.send();
+};
+
+// Action sur le bouton, qui appelle la fonction de requete AJAX
+boutonAskWeather.addEventListener('click', function() {
+	var actualStatus = 0;
+	askWeather(actualStatus);
 });
-
-
